bc36a0baa1de5762e477c70d8cd89e0420f7b637
Diego Brito <britodiego.tech@gmail.com>
2021-01-22 11:55:16 -0400

    update

--- server/build/models/base.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseModel {
-    constructor(sql) {
-        this.sql = sql;
-    }
-}
-exports.default = BaseModel;
